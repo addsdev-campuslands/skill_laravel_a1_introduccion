91c9c55fb658ad420743a377ff29a3936ec1e0b1
Adrian Ruiz <addsdeveloper@gmail.com>
2025-08-29 14:07:45 +0000

    feat: :sparkles: Add post and category migrations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDelete;
+use App\Models\Category;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDelete;
     protected $table = "posts";
 
-    protected $fillable = ['title', 'content', 'status'];
+    protected $fillable = [
+        'title', 'content', 'status',
+        'published_at', 'conver_image', 'tags', 'meta'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'tags' => 'array',
+        'meta' => 'meta'
+    ];
+
+    public function categories() {
+        // Tabla pivote post_category
+        return $this->belongsToMany(Category::class)->using(CategoryPost::class)->withTimestamps();
+    }
 }
